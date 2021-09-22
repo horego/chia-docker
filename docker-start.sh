c83dc29d9d2984e1d97c17bda5d7155f0fe64673
horego <40524748+horego@users.noreply.github.com>
2021-09-22 14:06:29 +0200

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#touch "$CHIA_ROOT/log/debug.log"
-#tail -n0 -f "$CHIA_ROOT/log/debug.log" &
+touch "$CHIA_ROOT/log/debug.log"
+tail -n0 -f --pid=$$ "$CHIA_ROOT/log/debug.log" &
 
 # shellcheck disable=SC2154
 if [[ ${farmer} == 'true' ]]; then
